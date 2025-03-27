4bf58f68a2b3f3a3fd1808fafe6617d35aae9433
Maksim Elizarev <44438314+djmaxus@users.noreply.github.com>
2025-03-27 15:13:06 +0000

    feat(matlab): check for code issues pre-2023b
    Signed-off-by: Maksim Elizarev <44438314+djmaxus@users.noreply.github.com>

--- buildfile.m
+++ buildfile.m
@@ -1,15 +1,21 @@
 function plan = buildfile()
+
 plan = buildplan(localfunctions);
+plan.DefaultTasks = ["check","test"];
 
 if isMATLABReleaseOlderThan("R2023b")
     plan("test") = matlab.buildtool.Task( ...
     Description="Run tests", ...
     Actions=@(~) assert(runtests().Failed == 0));
-    plan.DefaultTasks = "test";
+
+    plan("check") = matlab.buildtool.Task(...
+    Description="Identify code issues", ...
+    Actions=@(~) assert(isempty(codeIssues().Issues)));
+    
     return;
 end
 
 plan("check") = matlab.buildtool.tasks.CodeIssuesTask(WarningThreshold=0);
 plan("test") = matlab.buildtool.tasks.TestTask;
-plan.DefaultTasks = ["check","test"];
+
 end
