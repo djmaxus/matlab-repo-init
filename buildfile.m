672efe8726ca5fc05f7939adb7be97bccb09bd30
Maksim Elizarev <44438314+djmaxus@users.noreply.github.com>
2025-04-16 19:57:20 +0100

    fix(ci): issue filters in pre-R2023b checks
    Signed-off-by: Maksim Elizarev <44438314+djmaxus@users.noreply.github.com>

--- buildfile.m
+++ buildfile.m
@@ -3,6 +3,8 @@ function plan = buildfile()
 plan = buildplan(localfunctions);
 plan.DefaultTasks = ["check","test"];
 
+warning_threshold = 0;
+
 if isMATLABReleaseOlderThan("R2023b")
     plan("test") = matlab.buildtool.Task( ...
     Description="Run tests", ...
@@ -10,12 +12,20 @@ if isMATLABReleaseOlderThan("R2023b")
 
     plan("check") = matlab.buildtool.Task(...
     Description="Identify code issues", ...
-    Actions=@(~) assert(isempty(codeIssues().Issues)));
-    
+    Actions=@(~) assert(code_issues(warning_threshold)));
+
     return;
 end
 
-plan("check") = matlab.buildtool.tasks.CodeIssuesTask(WarningThreshold=0);
+plan("check") = matlab.buildtool.tasks.CodeIssuesTask(WarningThreshold=warning_threshold);
 plan("test") = matlab.buildtool.tasks.TestTask;
 
+end
+
+function flag = code_issues(warning_threshold)
+    issues = codeIssues().Issues;
+    disp(issues);
+    errors = find(issues.Severity == 'error');
+    warnings = find(issues.Severity == 'warning');
+    flag = (numel(errors) == 0) && (numel(warnings) <= warning_threshold);
 end
